0ba15b8064ef6809ca02789eeaed6fdacd88070e
YaKuBoVO <oyakubov526@gmail.com>
2023-07-09 15:30:03 +0500

    Sign-up tayyor

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
 "use strict"
+document.querySelector('.img-btn').addEventListener('click', function() {
+   document.querySelector('.cont').classList.toggle('s--sign-up');
+});
+
+
+
+
+
+
+
+
+//let aFormUp = document.querySelector('#formUp-container__a');
+//let bFormUp = document.querySelector('#formUp-container__b');
+
+//let parentLeft = document.querySelector('#container-left');
+
+//let aParentLeft = document.querySelector('#parent-left__a');
+//let bParentLeft = document.querySelector('#parent-left__b');
+//let allButtons = document.querySelectorAll('.submit');
+//let switchBtn = document.querySelectorAll('.switch-btn');
+//let leftBtn = document.querySelector('.btn-left');
+
+
+
+//leftBtn.addEventListener('click', (e) => {
+//   aParentLeft.classList.toggle('is-txr');
+//   aParentLeft.classList.add('is-hidden');
+//   bParentLeft.classList.add('is-txr');
+
+   
+//   aFormUp.classList.add('is-txl');
+//   aFormUp.classList.add('is-hidden');
+
+
+//   bFormUp.classList.add('is-txr');
+//   bFormUp.classList.add('.is-z200');
+
+   
+   
+//});
+
+
+
+//let getButtons = (e) => e.preventDefault();
+
+// let changeForm = (e) => {
+//   parentLeft.classList.add('is-gx');
+//   setTimeout(function() {
+//     parentLeft.classList.remove;
+//   }, 1500);
+
+
+//   parentLeft.classList.toggle('is-txr');
+
+//   bParentLeft.classList.toggle('is-hidden');
+//   aParentLeft.classList.toggle('is-hidden');
+//   aFormUp.classList.toggle('is-txl');
+//   bFormUp.classList.toggle('is-txl');
+//   bFormUp.classList.toggle('is-z200');
+//}
+
+//let mainF = (e) => {
+//   for(let i = 0; i < allButtons.length; i++) {
+//      allButtons[i].addEventListener('click', getButtons);
+//   }
+//   for(let i = 0; i < aParentLeft.length; i++) {
+//      allButtons[i].addEventListener('click', changeForm);
+//   }
+//}
+//window.addEventListener('load', mainF); 
